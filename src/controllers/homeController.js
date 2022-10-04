abf332b8131b8915788fb48208f1c80c6c2f7385
Võ Tấn Trung <tantrungse@gmail.com>
2022-10-04 17:01:10 +0700

    Kết Nối Tới MySQL Database với Sequelize

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,15 @@
-let getHomePage = (req, res) => {
+import db from '../models/index';
+
+let getHomePage = async (req, res) => {
+    try {
+        let data = await db.User.findAll();
+        return res.render('homepage.ejs', {
+            data: JSON.stringify(data)
+        });
+    } catch (e) {
+        console.log(e);
+    }
+
     return res.render('homepage.ejs');
 }
 
