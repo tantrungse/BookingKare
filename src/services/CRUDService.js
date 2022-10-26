071ac1aacfd53a8842f82104d79d03e50637d507
Võ Tấn Trung <tantrungse@gmail.com>
2022-10-26 23:49:31 +0700

    Sử Dụng ORM Để Hiển Thị Dữ liệu (Read)

--- src/services/CRUDService.js
+++ src/services/CRUDService.js
@@ -36,6 +36,20 @@ let hashUserPasswor = (password) => {
     })
 }
 
+let getAllUser = () => {
+    return new Promise(async(resolve, reject) => {
+        try {
+            let users = db.User.findAll({
+                raw : true,
+            });
+            resolve(users);
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
 module.exports = {
-    createNewUser: createNewUser
+    createNewUser: createNewUser,
+    getAllUser: getAllUser,
 }
