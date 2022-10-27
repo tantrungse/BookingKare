14eb5ecfe78d75bf62a2d747b6773597b6ef3847
Võ Tấn Trung <tantrungse@gmail.com>
2022-10-27 21:20:46 +0700

    Sử Dụng ORM Để Sửa Dữ liệu (Edit)

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -30,12 +30,30 @@ let postCRUD = async (req, res) => {
 
 let displayGetCRUD = async (req, res) => {
     let data = await CRUDService.getAllUser();
-    console.log('-----------------------');
-    console.log(data);
-    console.log('------------------------');
     
     return res.render('displayCRUD.ejs', {
         dataTable: data
+    });
+}
+
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+    console.log(userId);
+    if(userId) {
+        let userData = await CRUDService.getUserInfoById(userId);
+        return res.render('editCRUD.ejs', {
+            user: userData
+        });
+    } else {
+        return res.send('User not found');
+    }
+}
+
+let putCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data);
+    return res.render('displayCRUD.ejs', {
+        dataTable: allUsers
     });
 }
 
@@ -45,4 +63,6 @@ module.exports = {
     getCRUD: getCRUD,
     postCRUD: postCRUD,
     displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
 }
