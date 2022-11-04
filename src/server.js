d01cd563391e145434802bb6d577142e8a03d6c7
LAPTOP-G9CBJI3P\tantrungse <tantrungse@gmail.com>
2022-11-04 16:14:01 +0700

    Hoàn thiện chức năng đăng nhập

--- src/server.js
+++ src/server.js
@@ -3,9 +3,20 @@ import bodyParser from 'body-parser';
 import viewEngine from './config/viewEngine';
 import initWebRoutes from './route/web';
 import connectDB from './config/connectDB';
+import cors from 'cors';
+
 require('dotenv').config();
 
 let app = express();
+// app.use(cors({ origin: true }))
+app.use(
+    cors({
+      origin: true,
+      preflightContinue: true,
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      credentials: true,
+    })
+  )
 
 // config app
 
