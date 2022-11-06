8da4fb7139d2022272f4517af19e1a5d4d41a981
LAPTOP-G9CBJI3P\tantrungse <tantrungse@gmail.com>
2022-11-06 21:09:34 +0700

    Viết API Thêm, Sửa, Xóa Người Dùng

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -46,7 +46,33 @@ let handleGetAllUsers = async (req, res) => {
     })
 }
 
+let handleCreateNewUser = async (req, res) => {
+    let message = await userService.createNewUser(req.body);
+    console.log(message);
+    return res.status(200).json(message);
+}
+
+let handleEditUser = async (req, res) => {
+    let data = req.body;
+    let message = await userService.updateUser(data);
+    return res.status(200).json(message);
+}
+
+let handleDeleteUser = async (req, res) => {
+    if (!req.body.id) {
+        return res.status(200).json({
+            errCode: 1,
+            errMessage: "Missing required parameters!"
+        })
+    }
+    let message = await userService.deleteUser(req.body.id);
+    return res.status(200).json(message); 
+}
+
 module.exports = {
     handleLogin: handleLogin,   
-    handleGetAllUsers: handleGetAllUsers
+    handleGetAllUsers: handleGetAllUsers,
+    handleCreateNewUser: handleCreateNewUser,
+    handleEditUser: handleEditUser,
+    handleDeleteUser: handleDeleteUser
 }
