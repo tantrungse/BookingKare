9f2e4e288aff4af98f94f289f5f1f180a7c9ad68
LAPTOP-G9CBJI3P\tantrungse <tantrungse@gmail.com>
2022-12-15 21:13:35 +0700

    Import Data Từ EXCEL Và Viết API GET ALLCODE

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -69,10 +69,24 @@ let handleDeleteUser = async (req, res) => {
     return res.status(200).json(message); 
 }
 
+let getAllCode = async (req, res) => {
+    try {
+        let data = await userService.getAllCodeService(req.query.type);
+        return res.status(200).json(data);
+    } catch (e) {
+        console.log('Get all code errors: ', e);
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })
+    }
+}
+
 module.exports = {
     handleLogin: handleLogin,   
     handleGetAllUsers: handleGetAllUsers,
     handleCreateNewUser: handleCreateNewUser,
     handleEditUser: handleEditUser,
-    handleDeleteUser: handleDeleteUser
+    handleDeleteUser: handleDeleteUser,
+    getAllCode: getAllCode
 }
